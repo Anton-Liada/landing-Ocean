c53bcd2893f8155726f3ef6f4367ea7d19c36dda
Anton Liada <liadaantom@gmail.com>
2022-11-24 09:04:18 +0200

    hero & subscribe sections were implemented

---
+++ src/scripts/main.mjs
@@ -0,0 +1,22 @@
+'use strict';
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// const swiper = new Swiper('.swiper', {
+//   loop: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+
+const accordion = document.querySelectorAll('.accordion__content');
+
+accordion.forEach((element) => {
+  element.addEventListener('click', () => {
+    element.classList.toggle('accordion__content--active');
+  });
+});
