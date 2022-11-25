921091951c38e936559ebdfb4bc79881c45a34b6
Anton Liada <liadaantom@gmail.com>
2022-11-25 21:38:46 +0200

    accordion and card-swaiper were implemented

--- src/scripts/main.mjs
+++ src/scripts/main.mjs
@@ -2,18 +2,24 @@
 
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
+//swiper
 const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+const cardSwiper = new Swiper(".card-swiper", {
+  slidesPerView: 'auto',
 
   scrollbar: {
+    el: ".swiper-scrollbar",
     draggable: true,
-    el: '.swiper-scrollbar',
   },
 });
 
+//burger-menu
 window.addEventListener("hashchange", () => {
   if (window.location.hash === "#menu") {
     document.body.classList.add("page__body--with-menu");
@@ -22,14 +28,15 @@ window.addEventListener("hashchange", () => {
   }
 });
 
-const accordion = document.querySelectorAll(".accordion__content");
+// const accordion = document.querySelectorAll(".accordion__content");
 
-accordion.forEach((element) => {
-  element.addEventListener("click", () => {
-    element.classList.toggle("accordion__content--active");
-  });
-});
+// accordion.forEach((element) => {
+//   element.addEventListener("click", () => {
+//     element.classList.toggle("accordion__content--active");
+//   });
+// });
 
+// button-send
 document.querySelector("form").addEventListener("click", function (event) {
   event.preventDefault();
 });
