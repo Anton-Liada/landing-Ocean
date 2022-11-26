ccc5586ce1c3452b2958d04aa2c29e32df8b5f88
Anton Liada <liadaantom@gmail.com>
2022-11-26 21:54:35 +0200

    dark theme was implemented & hovers were added

--- src/scripts/main.mjs
+++ src/scripts/main.mjs
@@ -38,7 +38,23 @@ window.addEventListener("hashchange", () => {
 // });
 
 // button-send
-document.querySelector("form").addEventListener("click", function (event) {
+document.querySelector("form").addEventListener("click", (event) => {
   event.preventDefault();
 });
 
+const toggleSwitch = document.querySelector(".switch__input" );
+const toggleSwitchMenu = document.querySelector(".switch__input-menu");
+
+function switchTheme(e) {
+  console.log(e);
+  if(e.target.checked) {
+    document.body.classList.add('dark-theme');
+    // document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.body.classList.remove('dark-theme');
+    // document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+toggleSwitchMenu.addEventListener('change', switchTheme, false);
